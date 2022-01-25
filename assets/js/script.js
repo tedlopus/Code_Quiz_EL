089affa69120ad5227c67a8a8fa1a3d077da6fdf
Edward Lopus <tedlopus@gmail.com>
2022-01-25 16:16:27 -0700

    added more functions to the javascript and added changes to the css and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,95 @@
 var timerEl = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var quiz = document.querySelector(".quiz-container");
+var startButton = document.getElementById("start-btn");
 var start = document.getElementById("start");
-var quiz = document.getElementById("quiz");
-var questions = document.getElementById("questions");
-var choice = document.querySelector(".choices");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
-
+var questions = document.getElementById("question-container");
+var question = document.getElementById("question");
+var choice = document.getElementById("choice-buttons");
+var currentQuestion;
 var timer;
 var timerCount;
 
 let quizQuestions = [
-    {
-        questions: "Which is of the follwing is NOT a logical operator used in Javascript?",
-        choiceA: "&&",
-        choiceB: "%",
-        choiceC: "||",
-        choiceD: "!",
-        answer: "B",
-    },
-    {
-        questions: "What is an Array?",
-        choiceA: "An object that can store multiple values at once",
-        choiceB: "A set of statements that performs a task or calculates a value",
-        choiceC: "A procedure in JavaScript that waits for an event to occur",
-        choiceD: "A basic control statement that allows you to loop through the properties of an object",
-        answer: "A",
-    },
-    {
-        questions: "What is a valid event that you can use with an event listener?",
-        choiceA: "keypush",
-        choiceB: "mouseup",
-        choiceC: "null",
-        choiceD: "chick",
-        answer: "B",
-    },
-    {
-        questions: "If const cars = ['Ford', 'Audi', 'Subaru', 'Toyota']: then what is cars[3]?",
-        choiceA: "Subaru",
-        choiceB: "Ford",
-        choiceC: "Audi",
-        choiceD: "Toyota",
-        answer: "D",
-    },
+  {
+    question:
+      "Which is of the follwing is NOT a logical operator used in Javascript?",
+    choices: [
+      { text: "&&", correct: false },
+      { text: "%", correct: true },
+      { text: "||", correct: false },
+      { text: "!", correct: false },
+    ],
+  },
+  {
+    question: "What is an Array?",
+    choices: [
+      { text: "An object that can store multiple values at once", correct: true },
+      { text: "A set of statements that performs a task or calculates a value", correct: false },
+      { text: "A procedure in JavaScript that waits for an event to occur", correct: false },
+      { text: "A basic control statement that allows you to loop through the properties of an object", correct: false },
+    ],
+  },
+  {
+    question: "What is a valid event that you can use with an event listener?",
+    choices: [
+        { text: "keypush", correct: false },
+        { text: "mouseup", correct: true },
+        { text: "null", correct: false },
+        { text: "chick", correct: false },
+    ],
+  },
+  {
+    question: "If const cars = ['Ford', 'Audi', 'Subaru', 'Toyota']: then what is cars[3]?",
+    choices: [
+        { text: "Subaru", correct: false },
+        { text: "Ford", correct: false },
+        { text: "Audi", correct: false },
+        { text: "Toyota", correct: true },
+    ],
+  },
 ];
 
 function startGame() {
-    timerCount = 60;
-    startTimer()
+  startButton.classList.add('hide');
+  timerCount = 60;
+  startTimer();
+  initiateQuestion();
 }
 
 function startTimer() {
-    // Sets interval in variable
-    timer = setInterval(function() {
-      timerCount--;
-      timerEl.textContent = timerCount;
-      if(timerCount === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
-      }
-  
-    }, 1000);
-  }
+  // Sets interval in variable
+  timer = setInterval(function () {
+    timerCount--;
+    timerEl.textContent = timerCount;
+    if (timerCount === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timer);
+      // Calls function to create and append image
+      sendMessage();
+    }
+  }, 1000);
+}
+//Function that starts the questions after the user has clicked start quiz
+function initiateQuestion() {
+    nextQuestion(quizQuestions[currentQuestion]);
+    currentQuestion++;
+}
+// Function that sends the user to the next question
+function nextQuestion(q) {
+    questions.innerText = q.question;
+    for (let i = 0; i < q.choices.length; i++) {
+        choice.innerText = q.choices[i].text;
+
+        if (q.choices[i].correct) {
+            choice.dataset.correct = q.choices[i].correct;
+            console.log(choice.dataset.correct)
+        }
+    }
+}
 
 //Send a message for when the timer runs out - also populate the word
 function sendMessage() {
-    timerEl.textContent = "You're out of time!";
+  timerEl.textContent = "You're out of time!";
 }
 
 // Event listener that starts the game when the start game button is clicked.
